57b910318109efc57befcf40b23b2b038c5360d0
jpoliveira08 <joaopedro.oliveirajava@gmail.com>
2022-01-22 23:53:03 -0300

    Refactoring some views

--- App/Controllers/Register.php
+++ App/Controllers/Register.php
@@ -7,4 +7,13 @@ use Core\Controller;
 class Register extends Controller
 {
 
+    /**
+     * Show the register page
+     *
+     * @return void
+     */
+    public function indexAction(): void
+    {
+        
+    }
 }
