b7b353deff0e846780cad8c2d4c6695075e3977a
jpoliveira08 <joaopedro.oliveirajava@gmail.com>
2022-01-28 16:01:47 -0300

    first prototype of login page

--- public/js/login/main.js
+++ public/js/login/main.js
@@ -1,1 +1,11 @@
-console.log("Works");
+
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#password');
+
+togglePassword.addEventListener('click', function (e) {
+    //toogle the type attribute
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye slash icon
+    this.classList.toggle('fa-eye-slash');
+});
